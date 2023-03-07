78293383c0bf93e0a3cae2357b6813ca1dd4065a
Taisiya Tomashevska <taya22011985@gmail.com>
2023-03-07 21:41:09 +0200

    change formula

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,12 +30,13 @@ export class App extends Component {
   };
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
+    console.log(good)
     return good + neutral + bad;
   };
 
   countPositiveFeedbackPercentage = () => {
     const { good } = this.state;
-    return Math.round((good * 100) / this.countTotalFeedback());
+    return Math.round((good / this.countTotalFeedback()) * 100);
   };
 
   render() {
